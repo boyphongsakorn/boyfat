fe2307ab6915b241bf507374ddaa346d21d5ae03
Phongsakorn Wisetthon <boy1556@hotmail.com>
2018-12-26 13:38:27 +0700

    update

--- src/com/example/boyfat/MainActivity.java
+++ src/com/example/boyfat/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        button = (Button) findViewById(R.id.MyButton);
+        button = (Button) findViewById(R.id.button1);
         
 		button.setOnClickListener(new OnClickListener() {
 			public void onClick(View arg0) {
